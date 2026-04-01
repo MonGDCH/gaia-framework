37217ce8f8da37db1cb9573d4e07efd33a3b0039
monLam <985558837@qq.com>
2026-04-01 16:50:53 +0800

    commit

--- src/support/Plugin.php
+++ src/support/Plugin.php
@@ -188,12 +188,6 @@ class Plugin
      */
     protected static function init()
     {
-        // Plugin.php in vendor
-        $file = __DIR__ . '/../../../../../support/bootstrap.php';
-        if (is_file($file)) {
-            require_once $file;
-            return;
-        }
         require_once __DIR__ . '/bootstrap.php';
     }
 }
